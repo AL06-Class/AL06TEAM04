29ee4563df4ba4411624599fe56c8449471f2330
waterdog-log <__>
2026-07-09 10:48:19 +0900

    Update job posting creation flow

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import { CompanyDashboardPage } from "./pages/company/CompanyDashboardPage";
 import { JobPostCompletePage } from "./pages/company/JobPostCompletePage";
 import { JobPostCreatePage } from "./pages/company/JobPostCreatePage";
 import { JobPostManagePage } from "./pages/company/JobPostManagePage";
+import { JobPostDetailPage } from "./pages/JobPostDetailPage";
 
 const recommendedJobPostings = [
   {
@@ -110,14 +111,7 @@ export default function App() {
   }
 
   if (detailMatch) {
-    return (
-      <PendingRoutePage
-        description="공고 상세 보기 페이지는 현재 다른 협업자가 작업 중입니다."
-        href="/company/job-posts"
-        linkLabel="공고 관리로 이동"
-        title="공고 상세 보기 페이지 준비 중"
-      />
-    );
+    return <JobPostDetailPage jobPostingId={detailMatch[1]} />;
   }
 
   if (pathname === routes.home) {
